2c3b2a1db9373fbe57d6910807448ae683340557
Jenner Jr <jennerjj2000@gmail.com>
2026-09-25 15:10:33 -0300

    DeployFix-+Mudancas em relatorio sem Qc

--- reeferconecta/reeferbras/components/navbar.tsx
+++ reeferconecta/reeferbras/components/navbar.tsx
@@ -176,7 +176,7 @@ export default function Navbar() {
             <div className="shrink-0">
               <Image
                 alt="Reeferbras Logo"
-                src="icons/reeferbras-r-icon.svg"
+                src="icons/icon.svg"
                 onClick={() => router.push('/')}
                 width={50}
                 height={50}
